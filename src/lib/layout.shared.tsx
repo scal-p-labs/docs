8da18958928798a63b57e2ee660e4aceb75ca3bd
Carlos Edu Js <carloseduardogit@gmail.com>
2026-05-22 15:39:45 -0300

    ui: fixes names and add logo link

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,12 +1,19 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import { appName, gitConfig, logo } from './shared';
+import Image from 'next/image';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       // JSX supported
-      title: appName,
+      title: (
+        <div className="flex items-center gap-2">
+          <Image src={logo} alt="Logo" width={24} height={24} />
+          {appName}
+        </div>
+      ),
     },
+
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
 }
--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,11 +1,12 @@
-export const appName = 'My App';
+export const appName = 'Scal-p Docs';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
+export const logo = "https://raw.githubusercontent.com/scal-p-labs/assets/main/logo-transparent.svg"
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'scal-p-labs',
+  repo: 'scalp-docs',
   branch: 'main',
 };
