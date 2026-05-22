a695d446bdefcc1f73fb151c3bc323da98ccc824
Carlos Edu Js <carloseduardogit@gmail.com>
2026-05-22 14:45:25 -0300

    chore: fix lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter, Albert_Sans, Barlow, Source_Code_Pro } from 'next/font/google';
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/cn";
 
-const sourceCodeProSourceCodePro = Source_Code_Pro({subsets:['menu','cyrillic','cyrillic-ext','greek','greek-ext','latin','latin-ext','vietnamese'],weight:['200','300','400','500','600','700','800','900'],variable:'--font-source-code-pro'});
+const sourceCodeProSourceCodePro = Source_Code_Pro({subsets:['cyrillic','cyrillic-ext','greek','greek-ext','latin','latin-ext','vietnamese'],weight:['200','300','400','500','600','700','800','900'],variable:'--font-source-code-pro'});
 
-const barlowBarlow = Barlow({subsets:['menu','latin','latin-ext','vietnamese'],weight:['100','200','300','400','500','600','700','800','900'],variable:'--font-barlow'});
+const barlowBarlow = Barlow({subsets:['latin','latin-ext','vietnamese'],weight:['100','200','300','400','500','600','700','800','900'],variable:'--font-barlow'});
 
-const albertSansAlbertSans = Albert_Sans({subsets:['menu','latin','latin-ext'],weight:['100','200','300','400','500','600','700','800','900'],variable:'--font-albert-sans'});
+const albertSansAlbertSans = Albert_Sans({subsets:['latin','latin-ext'],weight:['100','200','300','400','500','600','700','800','900'],variable:'--font-albert-sans'});
 
 const inter = Inter({
   subsets: ['latin'],
