8665b650a0417cfa957d93067e1f1bd7866b2dd4
Carlos Edu Js <carloseduardogit@gmail.com>
2026-05-22 14:43:25 -0300

    chore: add styles and font pairing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
+import { Inter, Albert_Sans, Barlow, Source_Code_Pro } from 'next/font/google';
+import { cn } from "@/lib/utils";
+
+const sourceCodeProSourceCodePro = Source_Code_Pro({subsets:['menu','cyrillic','cyrillic-ext','greek','greek-ext','latin','latin-ext','vietnamese'],weight:['200','300','400','500','600','700','800','900'],variable:'--font-source-code-pro'});
+
+const barlowBarlow = Barlow({subsets:['menu','latin','latin-ext','vietnamese'],weight:['100','200','300','400','500','600','700','800','900'],variable:'--font-barlow'});
+
+const albertSansAlbertSans = Albert_Sans({subsets:['menu','latin','latin-ext'],weight:['100','200','300','400','500','600','700','800','900'],variable:'--font-albert-sans'});
 
 const inter = Inter({
   subsets: ['latin'],
@@ -8,7 +15,7 @@ const inter = Inter({
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={cn(inter.className, albertSansAlbertSans.variable, barlowBarlow.variable, sourceCodeProSourceCodePro.variable)} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
