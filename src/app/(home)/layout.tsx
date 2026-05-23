10a7b239c79d3090fcf63f7ca5c1c22e91bd6c5e
Carlos Edu Js <carloseduardogit@gmail.com>
2026-05-22 22:06:12 -0300

    chore: remove layout base home

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,5 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/lib/layout.shared';
-
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+  return <div className="">
+    {children}
+  </div>;
 }
